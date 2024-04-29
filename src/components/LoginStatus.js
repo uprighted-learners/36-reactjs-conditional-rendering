0207e6d88265230719b67f183c3445dc896fe2da
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-04-29 17:28:46 -0500

    basic conditional rendering

--- src/components/LoginStatus.js
+++ src/components/LoginStatus.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import { redirect } from 'next/dist/server/api-utils'
+import React, { useState } from 'react'
 
 export default function LoginStatus() {
+    // state to track if you're logged in or not
+    const [isLoggedIn, setIsLoggedIn] = useState(false)
+    console.log("Logged in?: ", isLoggedIn)
+
+    // function to toggle the login status
+    const toggleLogin = () => {
+        setIsLoggedIn(!isLoggedIn)
+    }
+
     return (
-        <div>LoginStatus</div>
+        <div>
+            <h1>Welcome to our page!</h1>
+            {isLoggedIn ? (
+                <h2>You are logged in!</h2>
+            ) : (
+                <h2>You are not logged in!</h2>
+            )}
+            <button onClick={toggleLogin}>
+                {isLoggedIn ? "Log out" : "Log in"}
+            </button>
+        </div>
     )
 }
